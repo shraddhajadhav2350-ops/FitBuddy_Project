3b4c31710149531eaa319aabb19e3ab524091577
shraddhajadhav2350-ops <shraddhajadhav2350@gmail.com>
2026-03-07 17:47:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def home():
     return "FitBuddy Project Running Successfully!"
 
-if __name__ == "__main__":
-    app.run()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
