9845b70c1bc823e268f4a7f718cb54f82597b89c
shraddhajadhav2350-ops <shraddhajadhav2350@gmail.com>
2026-03-10 21:59:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
+from flask import Flask, render_template
 
-import os
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
